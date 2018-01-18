abe7f04e5e3b63b662b01aefda1ba17c22545614
indegow <indegow@aol.com>
2018-01-18 13:06:22 -0800

    finished graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,34 +2,71 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.valueArrays = [];
+  this.edgeArrays = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.valueArrays.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.valueArrays.indexOf(node) !== -1) {
+    return true;
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var index = this.valueArrays.indexOf(node);
+  if (index !== -1) {
+    this.valueArrays.splice(index, 1);
+  }
+  for (var i = 0; i < this.valueArrays.length; i++) {
+    if (node > this.valueArrays[i]) {
+      this.removeEdge(this.valueArrays[i], node);
+    } else {
+      this.removeEdge(node, this.valueArrays[i]);
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+var tempArr = [fromNode, toNode].sort((a, b) => a - b);
+  for (var i = 0; i < this.edgeArrays.length; i++) {
+    if (this.edgeArrays[i][0] === tempArr[0] && this.edgeArrays[i][1] === tempArr[1]) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  var tempArr = [fromNode, toNode].sort((a, b) => a - b);
+  this.edgeArrays.push(tempArr);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var tempArr = [fromNode, toNode].sort((a, b) => a - b);
+  var tempIndx = 0;
+  for (var i = 0; i < this.edgeArrays.length; i++) {
+    if (this.edgeArrays[i][0] === tempArr[0] && this.edgeArrays[i][1] === tempArr[1]) {
+      tempIndx = i;
+    }
+  }
+  this.edgeArrays.splice(tempIndx, 1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  this.valueArrays.map(ele => cb(ele));
+
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -4,16 +4,40 @@ var Tree = function(value) {
 
   // your code here
   newTree.children = [];  // fix me
-
+  extend(newTree, treeMethods);
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var leaf = Tree(value);
+  this.children.push(leaf);
 };
 
 treeMethods.contains = function(target) {
+  var answer  = false;
+  function traverse(target) {
+    if (this.children.length === 0) {
+      if (this.value === target) {
+        answer = true;
+      }
+    }
+
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        answer = true;
+      } 
+    }
+  }
+  traverse.call(this, target);
+  return answer;
 };
 
 
@@ -21,3 +45,21 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+/* if (this.value === target) {
+  //   return true;
+  // }
+
+  
+
+  // if (this.children.length) {
+  //   for (var i = 0; i < this.children.length; i++) {
+  //     if (this.children[i].value === target) { 
+  //       return true; 
+  //     } else {
+  //       this.children[i].contains(target);
+  //     }
+  //   }
+  // } 
+
+  // return false;
+*/
