1964a5e5bc0863fca0cfae1c1f604a327e1fbe6e
indegow <indegow@aol.com>
2018-01-18 16:34:04 -0800

    finished hash until the last 2 blue dots

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,19 +7,65 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tempArray = this._storage.get(index);
+  var tempArr = [k, v];
+  var putChecker = false;
+  if (tempArray === undefined) {
+    tempArray = [];
+    this._storage.set(index, tempArray);
+  }
+  
+  for (var i = 0; i < tempArray.length; i++) {
+    if (tempArray[i][0] === k) {
+      putChecker = true;
+      tempArray[i][1] = v;
+    }
+  }
+  if (!putChecker) {
+    tempArray.push(tempArr);
+    this._storage.set(index, tempArray);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tempArray = this._storage.get(index);
+
+  for (var i = 0; i < tempArray.length; i++) {
+    if (tempArray[i][0] === k) {
+      return tempArray[i][1];
+    }
+  }
+  
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tempArray = this._storage.get(index);
+  for (var i = 0; i < tempArray.length; i++) {
+    if (tempArray[i][0] === k) {
+      tempArray.splice(i, 1);
+    }
+  }
 };
 
 
 
 /*
+
+  var tempArr = [k, v];
+  var tempArray = this._storage.get(index);
+  if (tempArray === null) {
+    tempArray = [];
+  }
+  for (var i = 0; i < tempArray.length; i++) {
+    if (tempArray[i][0] === k) {
+      tempArray[i] = tempArr;
+    } 
+  }
+  tempArray.push(tempArr);
+
+
  * Complexity: What is the time complexity of the above functions?
  */
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,40 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
+// using array to store
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+// check if item is in the set
+// if not, add
+// constant
+  var isIn = this.contains(item);
+  if (!isIn) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+// linear check
+  var isIn = false;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (item === this._storage[i]) {
+      isIn = true;
+    }
+  }
+  return isIn;
 };
 
 setPrototype.remove = function(item) {
+// linear splice
+  for (var i = 0; i < this._storage.length; i++) {
+    if (item === this._storage[i]) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
