9c1c230a68032688806c5d89ac8cdbff244e2838
indegow <indegow@aol.com>
2018-01-19 10:12:29 -0800

    finished basic requirement

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,33 @@
 var BinarySearchTree = function(value) {
+
+  var newTree = Object.create(BSTMethods);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
 };
 
+BSTMethods = {};
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+BSTMethods.insert = function(value) {
+  var newNode = BinarySearchTree(value);
+  if (value > this.value && this.right === null) { this.right = newNode; }
+  if (value < this.value && this.left === null) { this.left = newNode; }
+  if (value < this.value) { this.left.insert(value); }
+  if (value > this.value) { this.right.insert(value); }
+};
+
+BSTMethods.contains = function(val) {
+  if (this.value === val) { return true; }
+  if (val < this.value && this.left === null) { return false; }
+  if (val > this.value && this.right === null) { return false; }
+  if (val < this.value) { return this.left.contains(val); }
+  if (val > this.value) { return this.right.contains(val); }
+
+};
+
+BSTMethods.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) { this.left.depthFirstLog(cb); }
+  if (this.right) { this.right.depthFirstLog(cb); }
+};
